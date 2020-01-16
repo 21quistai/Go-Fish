ca0636a76c430988ae6d15e90004297141dbf01b
21quistai <59572996+21quistai@users.noreply.github.com>
2020-01-16 09:17:07 -0500

    Add files via upload
    Players now take their turn and everything is correctly lined up.

--- gfPlayer.js
+++ gfPlayer.js
@@ -58,9 +58,7 @@ function GFPlayer(player, playerName) {
       } //else
     }, //printHandSize
 
-    printPairArmount: function () {
-      // prints the amount of pairs each player has
-      //definitly could be more efficient
+    printPairArmount: function () { // prints the amount of pairs each player has
       //TODO: add an id to each for design?
       var x;
       if (this.p) { //player 0
@@ -83,69 +81,34 @@ function GFPlayer(player, playerName) {
     },
 
     chooseWho: function () {
-      if (player) { // This asks the player who they want to ask for a card
-        //console.log('  Who would you like to ask for a card');
-        this.choose = player;
-        this.checkChooseWho(); //checks to see if it is correct format
-        return this.choose;
-      } else {
-        this.choose = 'player ' + (Math.random() * 4 + 1); // chooses random
-        checkChooseWho();
-        //This section might be useful for a log
-        if (this.choose == 'player 4') choose = 'you'; //displays you for better readability
-        console.log('  They asked ' + choose + ' for a ');
-        return this.choose;
-      }
-    },
+        choose = 'player ' + Math.floor(Math.random() * 4);
+        this.checkChooseWho(); //makes sure the player doesn't ask himself for a  card
+        return choose;
+      },
 
     checkChooseWho: function () {
-      if (this.p) {
-        if (this.choose.substring(0, 7) != 'player ') {
-          console.log('Did you even read the Instructions?');
-          this.chooseWho();
-          //checks to see if the first part of the input is 'player'
-          //if it isn't than it reruns chooseWho
+        if (choose == this.n) {  //if(this.choose.equals(playerName))
+          console.log('hi');
+          this.chooseWho();//asks a random person
         } //if
+      }, //checkChooseWho
 
-        var i = this.choose.substring(7);
-        if (i >= 4) {
-          console.log('Thats not a player stupid');
-          this.chooseWho();
-          //checks the last part of the input to see if it is a number
-          // that is a player. if it isn't than it reruns chooseWho
-        } //if (i >= 4)
-      } else {
-        if (this.choose == this.n) {  //if(this.choose.equals(playerName))
-          this.choose = 'player ' + (Math.random() * 4 + 1); //asks a random person
-        } //if
-      } //else
-    }, //checkChooseWho
-
-    chooseCard: function (input) { // This is going to have to change to button inputs
-      if (player) { // if its the player
-        this.choose1 = input;
-        return this.choose1;
-      } else {
-        this.choose1 = (int)(Math.random() * 13 + 1); // asks for a random card
-        //console.log(this.choose1);
-        return this.choose1;
-      } //else
+    chooseCard: function () { // This is going to have to change to button inputs
+      choose1 = Math.floor(Math.random() * 13 + 1);
+      return choose1;
     }, //chooseCard
 
-    checkChooseCard: function () { //likely wont be used
+    checkChooseCard: function () { //checks to see if the input is an actual card
       if (!isNaN(sc)) choose1 = sc; //if the sc scanner isn't a number run again
       else {
         console.log("  Opps! That isn't a card. Try again.");
         chooseCard();
-
         //If the input isn't a number it reruns
       } //else
 
-      if (choose1 > 13) {
+      if (choose1 > 13) { //reruns if greater than 13
         console.log("  Opps! That isn't a card. Try again.");
         chooseCard();
-
-        //If the input isn't a number 1-13 it reruns
       } //if choose1 > 13
     }, //checkChooseCard
 
@@ -163,19 +126,16 @@ function GFPlayer(player, playerName) {
     }, //hasCard
 
     checkForBooks: function () {
-      this.hand.sort(); //Doesn't work
-      var x;
-      //var t = this.getBookCount();//don't need this
-      for (var i = 0; i < this.hand.length; i++) { // not sure if this works or not
-        for (var j = 0; j < this.hand.length; j++) { //TODO: while loop
-
+      var l; //l for log
+      for (var i = 0; i < this.hand.length; i++) {
+        for (var j = 0; j < this.hand.length; j++) {
+          //TODO: change this to a while loop in case there is a pair with the first number
           if (i != j && this.getHand()[i].getValue() == this.getHand()[j].getValue()) {
-            //// TODO: set variables for this.getHand()[i].getValue() and such
+            //// TODO: set variables for this.getHand()[i].getValue() and such?
             /*itterates through j and i until the the values of j and i equal each other.
              *When they equal each other, if its the player it removes the buttons
              *It then removes the card from the hand array of everyone and loops over again
              */
-
             if (this.p) {
               document.getElementById(
                 this.getHand()[j].getValue() + this.getHand()[j].getSuit()
@@ -183,17 +143,20 @@ function GFPlayer(player, playerName) {
               document.getElementById(
                 this.getHand()[i].getValue() + this.getHand()[i].getSuit()
               ).remove();
-              /*
-              x = document.createElement('span');
-              x.innerHTML = 'You had a pair of ' + this.getHand()[i].getValue() + 's';
-              document.body.appendChild(x);
-              */
+
+              //removes the card buttons^
+              l = document.createElement('p');
+              l.innerHTML = 'You got a pair of ' + this.getHand()[i].getValue() + 's';
+              l.scrollTop = l.scrollHeight;
+              document.getElementById('log').appendChild(l);
+
+              //logs that you got a pair ^
             } //if player
 
-            this.hand.splice(j, 1);
-            this.hand.splice(i, 1);
-            i = 0;
-            j = 0;
+            this.hand.splice(j, 1);  //removes the pair
+            this.hand.splice(i, 1);  //removes the pair
+            i = 0; //sets it back to 0 in case there are more than 1 pair
+            j = 0; //sets it back to 0 in case there are more than 1 pair
             this.bookCount++;
           } //if values == each other
         } //for j
@@ -201,7 +164,7 @@ function GFPlayer(player, playerName) {
     }, //checkForBooks
 
     sortHand: function () {
-      this.hand.sort();//TODO: fix
+      //this.hand.sort();//TODO: fix
 
       //TODO: find a way to sort the hand
       /* this is the old code
@@ -217,18 +180,18 @@ function GFPlayer(player, playerName) {
       */
     }, //sortHand
 
-    removeCard: function () {
+    removeCard: function () { //Useless?
       if (this.p)  document.getElementById(this.card).remove();
     },//removeCard
 
     goFish: function (card) {
-      this.hand.push(card);
+      this.hand.push(card); //adds the card to the players hand then displays it
       if (this.p) card.printGFCard();
     }, //goFish
 
-    getHandSize: function () {
+    getHandSize: function () { //Useless? same as printHandSize?
       var x = document.getElementById('player-hand-size');
-      x.setAttribute('value', 'hi'); //'You have' + this.hand.length + 'cards'
+      x.setAttribute('value', 'hi');
       document.body.appendChild(x);
     }, //getHandSize
 
--- main.js
+++ main.js
@@ -14,15 +14,17 @@ var playert; //temporary player
 
 function main() {
   this.startGame();
-  this.game();
+  //this.game();
 }
 
 // This does all the stuff that sets the game up  like dealing hands and printing them out
 function startGame() {
+  /*
   console.log("You are playing GO FISH. If you don't know the rules you're stupid");
   console.log('When you ask a player for a card use the format: player #.');
   console.log(
     'When you ask a card from a player use face cards as their number. eg:king = 13.');
+*/
 
   //adds the first 28 cards to everyone so they have 7 cards to start the game
   while (topCard <= 27) {
@@ -57,30 +59,47 @@ function setPlayer(input) {
 } //setPlayer
 
 function enter() {
+  var l;
+  l = document.createElement('p');
   card = cardt;
-  console.log(player);
-  console.log(card);
-  if (turn2 == 0)requestWhat(player, card, turn2);
+  if (turn2 == 0) {
+    //makes sure the player doesn't go multiple times in a row
+    requestWhat(player, card, 0);
+    turn2++;
+  }else {
+    l.innerHTML = "It's not your turn. press continue";
+    document.getElementById('log').scrollTop = document.getElementById('log').scrollHeight;
+    document.getElementById('log').appendChild(l);
+  }
+
 } //enter
-
+//file:///Users/21quistai/Downloads/GoFish/goFish.html
 function game() {
-  while (myDeck.getSize() > 0) {
-    for (var turn = 0; turn < 4; turn++) { // cycles through the different players turns
-      whosTurn(turn);
-      players[turn].checkForBooks();
-      if (turn != 0)requestWhat(players[turn].chooseWho(), players[turn].chooseCard(), turn);
-      else enter(); //if its the player it uses inputs else it uses random inputs
-      checkingForBooks();
-      if (turn == 0) {
-        for (var i = 0; i < 4; i++) { // prints out everyones hand on the players turn
-          players[i].printHandSize();
-          players[i].printPairArmount();    //players[i].printHand();
-        } //for i
-      } //if (turn == 0)
+  //while (true) { //players[turn2].hand.length >= 0 myDeck.getSize() > 0
+  // TODO: replace while loop with a button
+  console.log('WHY DOES THIS FREEZE!!!!!!!!!!!!');
+  var turn = -1;
 
-      turn2 = turn;
-    } //for turn
-  } //while (myDeck.getSize() > 0)
+  while (turn < 3) {  // cycles through the different players turns
+    turn++;
+    whosTurn(turn);
+    players[turn].checkForBooks();
+    if (turn != 0) {
+      requestWhat(players[turn].chooseWho(), players[turn].chooseCard(), turn);
+      if (turn == 3) {
+        turn = 0;
+        for (var i = 0; i < 4; i++) { //is this needed? it shoud print everyones hand already
+          players[i].printHandSize();// prints out everyones hand on the players turn
+          players[i].printPairArmount();
+        } //for i
+      }
+
+      turn++;
+    }
+
+    //else enter(); //if its the player it uses inputs else it uses random inputs
+  } //for turn
+  //}//while (myDeck.getSize() > 0)
 } //game
 
 function whosTurn(player) { // This just says whos turn it is
@@ -89,35 +108,39 @@ function whosTurn(player) { // This just says whos turn it is
 } //whosTurn
 
 function requestWhat(who, card, t) {
-  for (var i = 0; i < players.length; i++) {
+  //who is being asked / card is the card that is being asked for / t is who is asking
+  for (var i = 0; i < players.length; i++) {// TODO: useless? substring who and replace i
     if (players[i].n == who) { // makes sure i equals the player that is being asked
       if (players[i].hasCard(card)) { // Checks to see if that person has the card
         console.log('They have a: ' + card);//say they have the card
+        printLog(who, card, t, true);
         for (var j = 0; j < players[i].hand.length; j++) {
           // scrolls throught the players hand until it finds the card
           if (players[i].hand[j].getValue() == card) {
             // This is adding the wrong card to the hand
             if (players[i] == players[0]) {
               document.getElementById(players[i].getHand()[j].getValue() + players[i].getHand()[j].getSuit()).remove();
+              console.log(players[i].getHand()[j].getValue() + players[i].getHand()[j].getSuit());
             } //if (players[i] == players[0])
 
             if (players[t] == players[0]) {
               document.getElementById(players[t].getHand()[j].getValue() + players[t].getHand()[j].getSuit()).remove();
+              console.log(players[t].getHand()[j].getValue() + players[t].getHand()[j].getSuit());
             } //if (players[t] == players[0])
 
             players[i].hand.splice(j, 1);//removes the card from whoever was aked
             players[t].hand.splice(j, 1);// myDeck.getDeck()[]adds the card to the player who asked
             players[i].printHandSize();
             players[i].printPairArmount();
-            players[t].checkForBooks();  // then removes both cards they asked for
+            players[t].checkForBooks();  // //TODO: useless?
             players[t].printHandSize();
             players[t].printPairArmount();
-            break;
           } //if (players[i].hand[j].getValue() == card)
         } //for j
       }else {
         console.log("  They didn't have that card.");
-        if (players[t].player) console.log('  You drew a: ' + topCard); //This is broken
+        if (players[t].p) console.log('  You drew a: ' + topCard); //This is broken
+        printLog(who, card, t, false);
         players[t].goFish(myDeck.getDeck()[topCard]); //Adds a card to whoevers turn it was
         topCard++;
         players[t].printHandSize();
@@ -127,6 +150,32 @@ function requestWhat(who, card, t) {
     } //if (players[i].n == who)
   } //for i
 } //requestWhat
+
+function printLog(who, card, f, hasCard) {
+  //who is who is being asked
+  //card is what card is being asked for
+  //f is who who the ask is from t was taken
+  //has card is true if the player has the car
+  //creates a button
+  var l;
+  l = document.createElement('h3');
+  document.getElementById('log').appendChild(l);
+  if (f == 0) { // if it's the player
+    l.innerHTML = 'you asked player ' + who + ' for a ' + card;
+  } else {
+    l.innerHTML = 'player ' + f + ' asked player ' + who + ' for a ' + card;
+  }
+
+  l = document.createElement('p');
+  document.getElementById('log').appendChild(l);
+  if (hasCard == true) {
+    l.innerHTML = 'they have that card';
+  } else {
+    l.innerHTML = 'Go Fish';
+  }
+
+  document.getElementById('log').scrollTop = document.getElementById('log').scrollHeight;
+}
 
 function reDrawHand(who) { // This gives the player 4 more cards if they run out of cards
   for (var i = 0; i < 4; i++) {
